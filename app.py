fca94fe4de921ae4fcca21f19c1d94a62fae5fc8
sebastian <raynosebastian19@gmail.com>
2024-06-03 01:08:40 +0700

    first commit

---
+++ app.py
@@ -0,0 +1,43 @@
+import os
+from flask import Flask, jsonify, request
+from flask_cors import CORS
+import joblib
+
+app = Flask(__name__)
+CORS(app)
+
+# Load the trained model
+model = joblib.load('decision_tree_model.joblib')
+
+@app.route('/predict', methods=['POST','GET'])
+def predict():
+    try:
+        # Extract features from the form data
+        age = int(request.form['age'])
+        anaemia = int(request.form['anaemia'])
+        creatinine_phosphokinase = float(request.form['creatinine_phosphokinase'])
+        diabetes = int(request.form['diabetes'])
+        ejection_fraction = int(request.form['ejection_fraction'])
+        high_blood_pressure = int(request.form['high_blood_pressure'])
+        platelets = float(request.form['platelets'])
+        serum_creatinine = float(request.form['serum_creatinine'])
+        serum_sodium = int(request.form['serum_sodium'])
+        sex = int(request.form['sex'])
+        smoking = int(request.form['smoking'])
+        time = int(request.form['time'])
+        
+        # Make predictions using the model
+        prediction = model.predict([[age, anaemia, creatinine_phosphokinase, diabetes, 
+                                      ejection_fraction, high_blood_pressure, platelets, 
+                                      serum_creatinine, serum_sodium, sex, smoking, time]])
+        
+        # Return the prediction as JSON
+        if prediction[0] == 1:
+            return jsonify({'prediction': 1, 'message': 'The patient is predicted to die'})
+        else:
+            return jsonify({'prediction': 0, 'message': 'The patient is predicted to survive'})
+    except Exception as e:
+        return jsonify({'error': str(e)}), 400
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ test.js
@@ -0,0 +1,28 @@
+const myHeaders = new Headers();
+myHeaders.append("Content-Type", "application/x-www-form-urlencoded");
+
+const formdata = new FormData();
+formdata.append("age", "62");
+formdata.append("anaemia", "0");
+formdata.append("creatinine_phosphokinase", "231.0");
+formdata.append("diabetes", "0");
+formdata.append("ejection_fraction", "25");
+formdata.append("high_blood_pressure", "1");
+formdata.append("platelets", "253000.0");
+formdata.append("serum_creatinine", "0.9");
+formdata.append("serum_sodium", "140");
+formdata.append("sex", "1");
+formdata.append("smoking", "1");
+formdata.append("time", "10");
+
+const requestOptions = {
+  method: "POST",
+  headers: myHeaders,
+  body: formdata,
+  redirect: "follow"
+};
+
+fetch("http://127.0.0.1:5000/predict", requestOptions)
+  .then((response) => response.text())
+  .then((result) => console.log(result))
+  .catch((error) => console.error(error));
